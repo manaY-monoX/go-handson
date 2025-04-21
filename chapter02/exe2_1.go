6d638ccdc150f1574569802b524c941f9ea62fd1
YamashitaManato <m-yamashita@mono-x.com>
2025-04-21 02:03:16 +0000

    DOC: Add Readme

--- chapter02/exe2_1.go
+++ chapter02/exe2_1.go
@@ -9,18 +9,32 @@ import (
 func Exe2_1() {
 	var input string
 	fmt.Print("値1(整数)を入力してください->")
-	fmt.Scanln("Input: ", &input)
+	fmt.Scanln(&input)
 	// 文字列を整数に変換
-	value1, _ := strconv.Atoi(input)
+	value1, err := strconv.Atoi(input)
+	if err != nil {
+		fmt.Println("エラー: 整数を入力してください")
+		return
+	}
 
 	fmt.Print("値2(整数)を入力してください->")
-	fmt.Scanln("Input: ", &input)
+	fmt.Scanln(&input)
 	// 文字列を整数に変換
-	value2, _ := strconv.Atoi(input)
+	value2, err := strconv.Atoi(input)
+	if err != nil {
+		fmt.Println("エラー: 整数を入力してください")
+		return
+	}
 
 	fmt.Println("値1+値2 = ", value1+value2)
 	fmt.Println("値1-値2 = ", value1-value2)
 	fmt.Println("値1*値2 = ", value1*value2)
+
+	if value2 == 0 {
+		fmt.Println("エラー: 値2が0のため、除算と剰余演算はできません")
+		return
+	}
+
 	fmt.Println("値1/値2 = ", value1/value2)
 	fmt.Println("値1%値2 = ", value1%value2)
 }
